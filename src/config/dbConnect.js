f3913f3030e705579d30ff58af70da10ebe1716f
henock21 <henooksisay@gmail.com>
2025-11-05 16:27:29 +0300

    changed to atlas database

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,15 +1,18 @@
 const mongoose = require('mongoose');
-
-const dbConnect = async () => {
+ require('dotenv').config();
+ const dbConnect = async () => {
    try{
-    const connect = await mongoose.connect(process.env.URL);
+    const connect = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
    console.log(`Project is connected to MongoDB: `);
-   }
+   }        
   catch (err){
     console.log(err);
     process.exit(1);
   } 
-}
+}       
  
 module.exports = dbConnect;
            //  OR
